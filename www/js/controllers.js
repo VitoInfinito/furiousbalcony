99bdad11882fa6f4a8dc460a358ee80ab3bb5e85
Tomas Hasselquist <tomas@hasselquist.se>
2014-11-14 10:35:00 +0100

    Name set in gui and hiding menu until name is chosen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,158 @@
+var http = 'http://lethe.se:10600';
+var hasUsername = false;
+
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, User) {
-	$scope.user = User.get();
+.controller('DashCtrl', function($rootScope, $scope, SettingsService) {
+  $scope.showMap = true;
+  $scope.showList = false;
+  $rootScope.z = true;
+  $scope.test = function() {
+    $rootScope.showList = true;
+  }
+  $scope.shouldShow = function() {
+    return true;
+  }
+  $scope.dash = {
+    exampleFunction: function() {
+      alert($scope.dash.checkname);
+    },
+    checkUsername: function() {
+      if(!$scope.dash.checkname) {
+        $scope.dash.errormsg = "Please enter a name";
+      }else {
+        SettingsService.checkName($scope.dash.checkname)
+          .then(function(success) {
+              if(success.data === "taken") {
+                $scope.dash.errormsg = "Name " + $scope.dash.checkname + " was taken.";
+              }else {
+                SettingsService.setName($scope.dash.checkname);
+                $scope.dash.hasUsername = true;
+                hasUsername = true;
+                //Makes other tabs visible
+                $rootScope.hasUsername = true;
+              }
+            });
+      }
+    },
+    connected: false,
+    hasUsername: false,
+    errormsg: ""
+  };
+
+  if(hasUsername) {
+    $scope.dash.hasUsername = true;
+    $scope.dash.checkname = SettingsService.getName();
+  }
+
+  SettingsService.checkConnection()
+    .then(function(success) {
+        $scope.dash.connected = true;
+
+      });
+
+
 })
 
 .controller('SettingsCtrl', function($scope) {
 })
 
 .controller('GamesCtrl', function($scope, Games) {
-	var update = function(data) {
-		$scope.games = data;
-	};
-
-	$scope.reload = function() {
-		Games.fetchGames(update);
-	};
-
-	$scope.reload();
 
   var socket;
-  socket = io.connect('http://lethe.se:10600/lobby');
+
+	$scope.reload = function() {
+		Games.fetchGames()
+      .then(function(success) {
+        var games = success.data;
+        console.info('getGames returned ' + games.length + ' items');
+        $scope.games = games;
+      });
+	};
+
+  function initSocket() {
+    socket = io.connect(http + '/lobby');
+
+    socket.on('connect', function() {
+      console.info('lobby socket connect');
+    });
+    
+    socket.on('gameAdded', function(gameList) {
+      console.info('gameAdded');
+      $scope.$apply(function() {
+        $scope.games = gameList;
+      });
+    });
+  }
+
+	$scope.reload();
+  initSocket();
+
+
   
 })
 
-.controller('GameDetailCtrl', function($scope, $stateParams, Games) {
-	var findGame = function(gameId) {
-		var games = Games.getGames();
-      	if(typeof gameId !== 'undefined') {
-        	for(game in games) {
-          		if(gameId === games[game].id) {
-         		   	return games[game];
-          		}
-        	}
-      	}
-      	return null;
-    };
-
+.controller('GameDetailCtrl', function($scope, $stateParams, Game) {
     var update = function(data) { $scope.game = data; };
     $scope.getGame = function() {
-      Games.fetchGame($stateParams.gameId, update);
+      Game.fetchGame($stateParams.gameId, update)
+        .then(function(success) {
+          $scope.game = success.data;
+        });
     };
-    /*$scope.game = findGame(parseInt($stateParams.gameId));*/
 
     $scope.getGame();
 
-    var socket;
+    /*var socket;
 
     socket = io.connect('http://lethe.se:10600');
 
     socket.on('connect', function() {
       console.log('Client has connected to the server!');
       socket.emit('connectToGame', { gameId: 0, playerId: 1, playerName: "thisname" });
+    });*/
+
+    function initSocket() {
+      socket = io.connect(http + '/', {query: 'playerId=' + $routeParams.playerId});
+      
+      /*if(socket.socket.connected){
+        socket.emit('connectToGame', { gameId: $routeParams.gameId, playerId: $routeParams.playerId, playerName: GameService.playerName });
+      }*/
+      
+      socket.on('connect', function() {
+        console.info('game socket connect');
+        //socket.emit('connectToGame', { gameId: $routeParams.gameId, playerId: $routeParams.playerId, playerName: GameService.playerName });
+      });
+      
+      socket.on('updateGame', function(game) {
+        console.info('updateGame');
+        console.info(game);
+        $scope.$apply(function() {
+          renderGame(game);
+        });
+        
+      });
+      
+      socket.on('gameError', function(errorMsg) {
+        $scope.$apply(function() {
+          $scope.gameError = errorMsg;
+        });
+      });
+    };
+    
+    function joinGame() {
+      Game.joinGame($stateParams.gameId, "7", "testname")
+        .then(function(success) {
+          console.info("joinGame success");
+        renderGame(success.data);
+        initSocket();
+      },
+      function(error) {
+        console.info("joinGame error")
+        $scope.gameError = error.data.error;
     });
+  };
+
+  joinGame();
     
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,29 +1,7 @@
+var http = "http://lethe.se:10600";
+var user = Object();
+
 angular.module('starter.services', [])
-
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
-    }
-  }
-})
 
 .factory('Games', ['$http', 
   function($http) {
@@ -31,46 +9,53 @@ angular.module('starter.services', [])
     //Save the games for future detailed references
     var games = [];
 
-  return {
-    fetchGames: function(callback) {
-      $http.get('http://lethe.se:10600/list')
-        .success(function(data){
-          games = data;
-        }).finally(function() {
-          callback(games);
-        });
-    },
-    fetchGame: function(gameId, callback) {
-      var game;
-      $http.get('http://lethe.se:10600/getgamebyid?id=' + gameId)
-        .success(function(data){
-          game = data;
-        }).finally(function() {
-          callback(game);
-        });
-    },
-    getGames: function() {
-      return games;
-    },
-    get: function(gameId) {
-      return games[gameId];
+    return {
+      fetchGames: function() {
+          return $http.get('http://lethe.se:10600/list');
+      },
+      fetchGame: function(gameId) {
+        return $http.get('http://lethe.se:10600/getgamebyid?id=' + gameId);
+      },
+      getGames: function() {
+        return games;
+      },
+      get: function(gameId) {
+        return games[gameId];
+      }
     }
-  }
 }])
 
-.factory('Game', function() {
+.factory('Game', ['$http', 
+  function($http) {
 
-
-  return {
-    get: function() {
-      // Simple index lookup
-      return null;
-    },
-    getUsers: function() {
-      
+    return {
+      fetchGame: function(gameId) {
+        return $http.get('http://lethe.se:10600/getgamebyid?id=' + gameId);
+      },
+      joinGame: function(gameId, playerId, playerName) {
+        return $http.post(http + "/joingame", { gameId: gameId, playerId: playerId, playerName: playerName });
+      }
     }
-  }
-})
+}])
+
+.factory('SettingsService', ['$http', 
+  function($http) {
+    return {
+      setName: function(name) {
+        user.name = name;
+      },
+      getName: function() {
+        return user.name;
+      },
+      checkConnection: function() {
+        return $http.get(http + '/checkConnection');
+      },
+      checkName: function(name) {
+        return $http.get(http + '/checkName?name=' + name)
+      }
+
+    }
+}])
 
 .factory('User', function() {
   var user = { id: 0, name: 'Testname' };
