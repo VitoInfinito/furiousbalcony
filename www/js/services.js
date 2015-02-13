ffe3814636336926a8b56c25b524c5bf6dba9c44
Tomas Hasselquist <tomas@hasselquist.se>
2015-02-13 09:59:04 +0100

    Added v.0.0.3 APK file

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
-//var http = "http://lethe.se:10600";
-var http = 'http://127.0.0.1:10600';
+var http = "http://lethe.se:10600";
+//var http = 'http://127.0.0.1:10600';
 var user = Object();
 var socket;
 var version = '3';
@@ -124,6 +124,9 @@ angular.module('starter.services', [])
       },
       getUserOfId: function(id) {
         return $http.get(http + '/getuserofid?id=' + id);
+      },
+      getClientVersion: function() {
+        return version;
       }
 
     }
